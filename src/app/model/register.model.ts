b9c0fee51481ec8642fd5583d5038d049db1557c
hadil <hadilgy25@gmailo.com>
2022-02-22 20:33:16 +0100

    hadil changes

---
+++ src/app/model/register.model.ts
@@ -0,0 +1,9 @@
+export class Register {
+    cin : Number =0;
+    nom : String ="";
+    prenom : String ="";
+    email : String ="";
+    tel : Number =0;
+    mdp : String ="";
+    vs : String ="";
+}
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { observable } from 'rxjs';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Register } from '../model/register.model';
+const httpOptions ={
+  headers:new HttpHeaders({'content_type':'application/json'})
+};
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  api:String ="url";
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
   }
